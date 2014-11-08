ed92b1a3e15b7bc417d6c0c1bb121562eafbf0df
Lior <liorshahverdi@gmail.com>
2014-11-08 12:17:58 -0500

    2nd commit init fixes

--- t1.cpp
+++ t1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <stdio>
 
 int main() {
 int sum = 0;
   for (int k=1; k<11; k++)   {
     sum += k;
   }
-  std::cout << "The sum is: " << sum << "\n";
+  cout << "The sum is: " << sum << "\n";
   return 0;
 }
